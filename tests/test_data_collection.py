a152a22163127f6c36ce910c323b1b8c7b521b8c
Steve Leung <4917419+stevenaleung@users.noreply.github.com>
2022-02-03 21:25:12 -0800

    Created test_get_post()

--- tests/test_data_collection.py
+++ tests/test_data_collection.py
@@ -1,8 +1,14 @@
-from src.util import get_reddit_connection
-
-
 import praw
+from src.util import *
+
 
 def test_connection():
     reddit = get_reddit_connection()
     assert isinstance(reddit, praw.reddit.Reddit)
+
+
+def test_get_post():
+    reddit = get_reddit_connection()
+    post_id = "d6xoro"
+    post = get_post(reddit, post_id)
+    assert isinstance(post, praw.reddit.Submission)
