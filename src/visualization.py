91d4ff876933244691fdb9b4f1622143f99a18f2
Steve Leung <4917419+stevenaleung@users.noreply.github.com>
2022-03-01 22:26:16 -0800

    Created create_tlc_ranking_figure()

--- src/visualization.py
+++ src/visualization.py
@@ -23,3 +23,30 @@ def create_tlc_analysis_figure(summary, num_hours_cutoff):
     plt.title("Sentiment of top level comments")
     plt.legend()
     return fig_handle
+
+
+def create_tlc_ranking_figure(summary, num_comments):
+    scores = summary["scores"][1:num_comments]
+    nta = summary["nta"][1:num_comments]
+    yta = summary["yta"][1:num_comments]
+    info = summary["info"][1:num_comments]
+    other = summary["other"][1:num_comments]
+
+    nta_idxs = np.where(nta)[0]
+    yta_idxs = np.where(yta)[0]
+    info_idxs = np.where(info)[0]
+    other_idxs = np.where(other)[0]
+
+    fig_handle = plt.figure()
+    plt.scatter(nta_idxs+1, scores[nta_idxs], c="tab:green", label="NTA")
+    plt.scatter(yta_idxs+1, scores[yta_idxs], c="tab:orange", label="YTA")
+    plt.scatter(info_idxs+1, scores[info_idxs], c="tab:purple", label="INFO")
+    plt.scatter(other_idxs+1, scores[other_idxs], c="tab:blue", label="Other")
+    plt.xlim((0,num_comments))
+    plt.grid("on")
+    plt.xlabel("Comments ranked by 'best'")
+    plt.ylabel("Voting score")
+    plt.title("Sentiment of 'best' ranked top level comments")
+    plt.legend()
+    plt.xticks(range(1,num_comments,2))
+    return fig_handle
