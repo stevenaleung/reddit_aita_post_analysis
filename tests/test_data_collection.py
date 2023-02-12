6df6eabc799cb3bc82a564f69d76cb76a1d7c607
Steve Leung <4917419+stevenaleung@users.noreply.github.com>
2023-02-11 16:58:31 -0800

    Update namespaces in the unit tests

--- tests/test_data_collection.py
+++ tests/test_data_collection.py
@@ -1,14 +1,14 @@
 import praw
-from src.util import *
+import src.util as util
 
 
 def test_connection():
-    reddit = get_reddit_connection()
+    reddit = util.get_reddit_connection()
     assert isinstance(reddit, praw.reddit.Reddit)
 
 
 def test_get_post():
-    reddit = get_reddit_connection()
+    reddit = util.get_reddit_connection()
     post_id = "d6xoro"
-    post = get_post(reddit, post_id)
+    post = util.get_post(reddit, post_id)
     assert isinstance(post, praw.reddit.Submission)
