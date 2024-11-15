432ef6575ba812ff5a8b6e2841b34112c1ab5001
Steve Leung <4917419+stevenaleung@users.noreply.github.com>
2024-11-14 19:42:27 -0800

    Create total score figure (#9)

--- README.md
+++ README.md
@@ -9,9 +9,6 @@ A package to analyze posts from reddit.com/r/AmItheAsshole/
 ## Quick start
 
 ```python
-%load_ext autoreload
-%autoreload 2
-
 import reddit_aita_post_analysis.util as util
 import reddit_aita_post_analysis.visualization as vis
 import matplotlib.pyplot as plt
@@ -20,13 +17,25 @@ post_id = "123456"
 
 reddit = util.get_reddit_connection()
 post = util.get_post(reddit, post_id)
-summary = util.analyze_post_tlc(post)
 
+summary = util.analyze_post_tlc(post)
+post_df = util.to_dataframe(post)
+```
+
+Create scatter plots to show top level judgements 
+```python
 num_hours_cutoff = 12
 fig = vis.create_tlc_analysis_figure(summary, num_hours_cutoff)
 plt.show()
 
 num_comments = 20
 fig = vis.create_tlc_ranking_figure(summary, num_comments)
+plt.show()
+```
+
+Create bar charts to show total judgement scores
+```python
+num_comments = 9
+fig = vis.create_total_score_figure(post_df, num_comments)
 plt.show()
 ```
--- reddit_aita_post_analysis/visualization.py
+++ reddit_aita_post_analysis/visualization.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def create_tlc_analysis_figure(summary, num_hours_cutoff):
@@ -70,3 +71,38 @@ def create_tlc_ranking_figure(summary, num_comments):
     plt.legend()
     plt.xticks(range(1, num_comments, 2))
     return fig_handle
+
+
+def create_total_score_figure(post_df: pd.DataFrame, num_tlc: int) -> mpl.figure.Figure:
+    """Create a number of bar charts that tally the score of every comment in the
+    comment tree. The user can specify the number of comments to analyze"""
+    df_grouped = post_df.groupby(["tlc_idx", "judgement"]).sum()
+
+    fig_handle = plt.figure()
+    # adjust the layout of the figure depending on the number of top level comments
+    # specified. by default, the figure will add columns before it adds rows
+    num_cols = int(np.ceil(np.sqrt(num_tlc)))
+    num_rows = int(np.ceil(num_tlc / num_cols))
+
+    for tlc_idx in range(num_tlc):
+        comment_scores_summed = df_grouped.loc[tlc_idx]["comment_score"]
+        plt.subplot(num_rows, num_cols, tlc_idx + 1)
+        create_total_score_plot(comment_scores_summed)
+
+    fig_handle.autofmt_xdate()
+    plt.tight_layout()
+    return fig_handle
+
+
+def create_total_score_plot(comment_scores_summed: pd.Series) -> None:
+    # when performing a groupby, the values in the pandas series index are not sorted.
+    # they are ordered is by first occurrence instead. we'd like to assign certain
+    # colors to certain judgements, so we need the order to be consistent
+    judgement_scores = {"NTA": 0, "YTA": 0, "UNCLEAR": 0, "INFO": 0}
+    for judgement in comment_scores_summed.index:
+        judgement_scores[judgement] = comment_scores_summed.loc[judgement]
+
+    bar_colors = ["tab:green", "tab:orange", "tab:blue", "tab:purple"]
+
+    plt.bar(judgement_scores.keys(), judgement_scores.values(), color=bar_colors)
+    plt.ylabel("Voting score")
